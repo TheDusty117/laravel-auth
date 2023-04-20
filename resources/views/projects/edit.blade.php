a8e59f954a1405fa94f881ee41a6de668f81e672
TheDusty117 <gabrieledipassio@gmail.com>
2023-04-20 18:04:56 +0200

    FEAT Views created inside projects

---
+++ resources/views/projects/edit.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Project EDIT</h1>
+@endsection
---
+++ resources/views/projects/show.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Project SHOW</h1>
+@endsection
