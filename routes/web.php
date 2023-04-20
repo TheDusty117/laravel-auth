ecc9456e789569e5298529b02fc027fbd81250a6
TheDusty117 <gabrieledipassio@gmail.com>
2023-04-20 18:00:34 +0200

    FEAT plugged Project Route to web php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
+//importo il controller
+use App\Http\Controllers\ProjectController;
+
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -26,6 +30,10 @@ Route::middleware('auth')->group(function () { //queste rotte puo' usarle solo L
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+    Route::resource('Projects', ProjectController::class);
+
 });
 
 require __DIR__.'/auth.php';
