f9e34fa3b51539ba7b0b9aae8fb33c4ccb5348bd
TheDusty117 <gabrieledipassio@gmail.com>
2023-04-20 18:43:32 +0200

    FEAT table created in index with btns

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,5 +1,56 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Project INDEX</h1>
+    <div class="container py-5">
+        <div class="d-flex align-items-center">
+            <h1 class="me-auto">Tutti i progetti</h1>
+
+            <div>
+                <a class="btn btn-sm btn-primary" href="{{ route('projects.create') }}">Nuovo Progetto</a>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="container">
+        <table class="table table-striped table-inverse table-responsive">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>Client</th>
+                    <th>Description</th>
+                    <th>Slug</th>
+                    <th>Data creazione</th>
+                    <th>Data modifica</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($projects as $project)
+                    <tr>
+                        <td>{{ $project->id }}</td>
+                        <td>
+                            <a href="{{ route('projects.show',$project) }}">{{ $project->title }}</a>
+                        </td>
+                        <td>{{ $project->client }}</td>
+                        <td>{{ $project->description }}</td>
+                        <td>{{ $project->slug }}</td>
+                        <td>{{ $project->created_at }}</td>
+                        <td>{{ $project->updated_at }}</td>
+                        <td>
+                            <div class="d-flex">
+                                <a class="btn btn-sm btn-secondary" href="{{ route('projects.edit',$project) }}">Edit</a>
+                            </div>
+                        </td>
+
+                    </tr>
+                @empty
+                    <tr>
+                        <th colspan="6">Nessun project trovato</th>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
 @endsection
